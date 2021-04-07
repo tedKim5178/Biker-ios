0d75533ef59684ce40c91a775b0782036e73a4a3
MoonkiKim(김문기) <rlaansrl789@gmail.com>
2021-04-07 15:23:47 +0900

    feature(n-map) : add naver map

--- Biker/ViewController.swift
+++ Biker/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import NMapsMap
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+    
+        let coor = locationManager.location?.coordinate
+        print("lat = \(coor?.latitude)")
+        print("lng = \(coor?.longitude)")
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("\(locations)")
+    }
 }
 
